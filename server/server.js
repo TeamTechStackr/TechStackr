53c4c25da9a532a8f4b41212e5e974066c3d096d
ejwoods <ewoods08@gmail.com>
2023-12-04 17:56:51 -0500

    set up boilerplate

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const exp = require('constants');
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3000;
+
+console.log('server reached')
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+module.exports = app;
