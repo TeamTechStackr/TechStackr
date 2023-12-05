7360c4bd6f859e1928c40e5900920897537195b8
ejwoods <ewoods08@gmail.com>
2023-12-05 18:11:09 -0500

    add postgres model and controllers, add pg package

---
+++ server/controllers/dbController.js
@@ -0,0 +1,27 @@
+const db = require('../models/tableModel');
+
+const dbController = {};
+
+dbController.addProject = (req, res, next) => {
+  console.log('addProject reached');
+  const query = `INSERT INTO projects (name, ghlink) VALUES ('TechStackr', 'https://github.com/TeamTechStackr/TechStackr/tree/main');` 
+  db.query(query)
+    .then((data) => {
+      console.log(data)
+    })
+      .catch((err) => {
+          console.log(err);
+          next({
+              log: `dbController.addProject ERROR : ${err}`,
+              message: {
+                  err: 'Error ocurred in db.Controller.addProject.',
+              },
+          });
+  });
+};
+
+dbController.getProjects = (req, res, next) => {
+
+}
+
+module.exports = dbController;
