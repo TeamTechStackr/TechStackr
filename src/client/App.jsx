3e615bc85a2ff22c0144b8bb0f746eb7d3d3f44f
Fredy Rubio <fredy93rubio@gmail.com>
2023-12-05 11:56:34 -0600

    add  omponent and container folders, basic app

---
+++ src/client/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        //render a container
+        <h1>TechStackr</h1>
+    )
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import App from './app';
+import App from './client/App';
 
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement);
 
-root.render(<h1>we are testing yippeer!</h1>);
+root.render(<App/>);
