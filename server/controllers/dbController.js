f47d809f24980039809e3b1e56b733f47712d00e
ejwoods <ewoods08@gmail.com>
2023-12-06 12:57:56 -0500

    update addProject middleware to pull from req body, make INSERT query, save project_id to res.locals

--- server/controllers/dbController.js
+++ server/controllers/dbController.js
@@ -4,21 +4,44 @@ const dbController = {};
 
 dbController.addProject = (req, res, next) => {
   console.log('addProject reached');
-  const query = `INSERT INTO projects (name, ghlink) VALUES ('TechStackr', 'https://github.com/TeamTechStackr/TechStackr/tree/main');` 
+  console.log('req body received', req.body);
+  const { name, ghLink } = req.body;
+  const query = `INSERT INTO projects (name, ghlink) VALUES ('${name}', '${ghLink}') RETURNING *;` 
   db.query(query)
     .then((data) => {
-      console.log(data)
+      console.log('id from query response', data.rows[0].project_id)
+      res.locals.projectId = data.rows[0].project_id
       return next();
     })
-      .catch((err) => {
-          console.log(err);
-          next({
-              log: `dbController.addProject ERROR : ${err}`,
-              message: {
-                  err: 'Error ocurred in db.Controller.addProject.',
-              },
-          });
-  });
+    .catch((err) => {
+        console.log(err);
+        next({
+            log: `dbController.addProject ERROR : ${err}`,
+            message: {
+                err: 'Error ocurred in db.Controller.addProject.',
+            },
+        });
+    });
+};
+
+dbController.addTechnologies = (req, res, next) => {
+  console.log('addTechnologies reached');
+  const { technologies } = req.body;
+  const techIds = [];
+  for (let i = 0; i < technologies.length; i++) {
+    const query = `INSERT INTO technologies (name) VALUES ${technologies[i]}`
+    db.query(query)
+      .then((data) => {
+        console.log(data);
+        return next();
+      })
+  }
+  
+
+};
+
+dbController.addProjectTechnologies = (req, res, next) => {
+  return next();
 };
 
 dbController.getProjects = (req, res, next) => {
